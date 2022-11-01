bc6b54d2fd9487cc65956ed7e8d1a1654768d8d7
lambn101 <buinguyenlam@gmail.com>
2022-11-01 19:43:33 +0100

    Create Martikelnummer.java

---
+++ Martikelnummer.java
@@ -0,0 +1,18 @@
+import javax.swing.JOptionPane;
+class Martikelnummer{
+	public static void main(String args[]){
+		int martikelNr = Integer.parseInt(JOptionPane.showInputDialog("Geben Sie Ihre Martikel-Nr ein"));
+		int hilf = martikelNr;
+		int querSum = 0;
+		
+		while (hilf > 0) { 
+			int i = hilf%10;
+			querSum += i;
+			hilf = hilf / 10;
+		}
+		
+		JOptionPane.showMessageDialog(null, "Ihre Martikel-Nr ist: " + martikelNr + "\nDie Quersumme davon ist: " + querSum);
+		
+	}
+}
+				
